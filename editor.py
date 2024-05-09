65b6fdb498981ca6e00090f4b590ea819002fe41
Caleb <calebraddatz06@gmail.com>
2024-05-08 23:32:03 -0500

    update

--- editor.py
+++ editor.py
@@ -3,64 +3,103 @@ from pygame.math import Vector2 as vector
 from settings import *
 from pygame.mouse import get_pressed as mouse_buttons
 from pygame.mouse import get_pos as mouse_pos
+from menu import Menu
+
 # Origin is a vector with (x,y) and every element relative to it
 class Editor:
-  def __init__(self):
+    def __init__(self):
 
-    # main setup
-    self.display_surface = pygame.display.get_surface()
-    self.pan_active = False
-    self.pan_offset = vector()
+        # main setup
+        self.display_surface = pygame.display.get_surface()
+        self.pan_active = False
+        self.pan_offset = vector()
 
-    #navigation
-    self.origin = vector()
+        #navigation
+        self.origin = vector()
 
-  #input
-  def event_loop(self):
-    # event looop
-    # close the game
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
+        #Support lines
+        self.support_line_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.support_line_surf.set_colorkey('green')
+        self.support_line_surf.set_alpha(30)
+
+        # selection
+        self.selection_index = 2
+
+        # menu
+        self.menu = Menu()
+
+    #input
+    def event_loop(self):
+        # event looop
+        # close the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
               pygame.quit()
               sys.exit()
-          self.pan_input(event)
+            self.pan_input(event)
+            self.selection_hotkeys(event)
+            self.menu_click(event)
 
-  def pan_input(self, event):
+    def pan_input(self, event):
 
-    # middle mouse button pressed or released
-    if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
-      self.pan_active = True
-      self.pan_offset = vector(mouse_pos()) - self.origin
-    if not mouse_buttons()[1]:
-      self.pan_active = False
+        # middle mouse button pressed or released
+        if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
+            self.pan_active = True
+            self.pan_offset = vector(mouse_pos()) - self.origin
+        if not mouse_buttons()[1]:
+            self.pan_active = False
 
-    # panning update
-    if self.pan_active:
-      self.origin = vector(mouse_pos()) - self.pan_offset
+        # panning update
+        if self.pan_active:
+            self.origin = vector(mouse_pos()) - self.pan_offset
 
-    if event.type == pygame.MOUSEWHEEL:
-      if pygame.key.get_pressed()[pygame.K_LCTRL]:
-        self.origin.y -= event.y*50
-      else:
-        self.origin.x -= event.y * 50
+        if event.type == pygame.MOUSEWHEEL:
+            if pygame.key.get_pressed()[pygame.K_LCTRL]:
+                self.origin.y -= event.y * 50
+            else:
+                self.origin.x -= event.y * 50
 
-  # Drawing
-  def draw_tile_lines(self):
-    cols = WINDOW_WIDTH // TILE_SIZE
-    rows = WINDOW_HEIGHT // TILE_SIZE
+    def selection_hotkeys(self,event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.selection_index += 1
+            if event.key == pygame.K_LEFT:
+                self.selection_index -= 1
+        self.selection_index = max(min(self.selection_index, 18),2)
 
-    origin_offset = vector(
-      x = self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
-      y = self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE
-    )
-    for col in range(cols + 1):
-      x = origin_offset.x + col * TILE_SIZE
-      pygame.draw.line(self.display_surface,LINE_COLOR, (x,0), (x,WINDOW_HEIGHT))
-  def run(self, dt):
-    
-    self.event_loop()
-
+    def menu_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
+            self.selection_index = self.menu.click(mouse_pos(),mouse_buttons())
     # Drawing
-    self.display_surface.fill('white')
-    self.draw_tile_lines()
-    pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
+    def draw_tile_lines(self):
+        cols = WINDOW_WIDTH // TILE_SIZE
+        rows = WINDOW_HEIGHT // TILE_SIZE
+
+        origin_offset = vector(
+            x=self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
+            y=self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE
+        )
+        self.support_line_surf.fill('green')
+        for col in range(cols + 1):
+            x = origin_offset.x + col * TILE_SIZE
+            pygame.draw.line(self.support_line_surf, LINE_COLOR, (x, 0), (x, WINDOW_HEIGHT))
+
+        for row in range(rows + 1):
+            y = origin_offset.y + row * TILE_SIZE
+            pygame.draw.line(self.support_line_surf, LINE_COLOR, (0, y), (WINDOW_WIDTH, y))
+
+        self.display_surface.blit(self.support_line_surf, (0, 0))
+
+    def run(self, dt):
+
+        self.event_loop()
+
+        # Drawing
+        self.display_surface.fill('grey')
+        self.draw_tile_lines()
+        pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
+        self.menu.display(self.selection_index)
+
